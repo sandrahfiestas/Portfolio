155062064f9e1930b8069c0a6cad427c4f4c80f5
sandrahfiestas <shfiestas@gmail.com>
2020-09-09 20:46:03 -0500

    mostrando data del json

---
+++ src/Component/Projects/Projects.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Projects.css';
+
+
+function Projects(){
+  const [projects, setProjects] = useState({});
+  useEffect(() => {
+      axios.get('https://backend-portfolio-shfiestas.herokuapp.com/portfolio')
+      .then(
+          res => setProjects(res.data)
+      )}, [setProjects])
+
+              {/* <div className="subtitle">
+                <h2>Skills</h2>
+                <ul className="about-skills">
+                    {data.skills && data.skills.map((skill, index) =>
+                        <p className="skill">{skill}</p>
+                    )}
+                </ul>
+            </div> */}
+
+  return(
+    <section className="container-projects">
+    <h2 className="subtitle">Proyectos</h2>
+ 
+      <div className="main-card">
+        {projects.length && projects.map((project, index) => 
+          <div className="card-project">
+
+            <div className="card-image">
+              <img src={project.image} className="img-project" alt={project.title}></img>
+            </div>
+          
+            <div className="card-information">
+              <div>
+                <h1>{project.title}</h1>
+                <h3>{project.name}</h3>
+                <p className="p-information">{project.description}</p>
+                <p className="p-information">{project.technology}</p>
+              </div>
+              <div>
+                <button className="btn-project"><a target="_blank"  href={project.linkWeb}>DEMO</a></button>
+                <button className="btn-project"><a target="_blank"  href={project.linkRepo}>CÓDIGO</a></button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+ 
+    </section>
+  )
+  };
+
+  export default Projects;
