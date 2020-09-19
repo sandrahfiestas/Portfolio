b25a68a1644c90ac83bddf89fbb1a2af65033e58
sandrahfiestas <shfiestas@gmail.com>
2020-09-19 17:31:44 -0500

    comentando menú hamburguesa

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,23 +1,21 @@
-
 import React  from 'react';
-import './Navbar.css';
+import './Navbar.scss';
 
 function Navbar(){
-
   return(
     <nav>
-    <input type="checkbox" id="check"/>
-    <label for="check" className="checkbtn">
-    <img src={require('../../images/menu_burger.png')} className="fas fa-bars btn-burger" alt="burger" />
-    </label>
-    {/* <label className="logo">SJHF</label> */}
+      {/* <input type="checkbox" id="check"/>
+      <label for="check" className="checkbtn">
+        <img src={require('../../images/menu_burger.png')} className="fas fa-bars btn-burger" alt="burger" />
+      </label> */}
+      <a className="a-logo" href="#"><img src={require('../../images/logo.png')} className="logo" alt="logo" /></a>
       <ul className="uli">
-        <li><a className="" href="#about">ABOUT</a></li>
-        <li><a href="#project">PROJECTS</a></li>
-        <li><a href="#contact">CONTACT</a></li>
+        <li><a href="#about">SOBRE MI</a></li>
+        <li><a href="#project">PROYECTOS</a></li>
+        <li><a href="#contact">CONTACTO</a></li>
       </ul>
     </nav>
-) 
+  ) 
 };
 
 export default Navbar;
