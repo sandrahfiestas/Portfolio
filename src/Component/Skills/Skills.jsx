52476d9c6d25208707c8356b6e36cee9b644f30a
sandrahfiestas <shfiestas@gmail.com>
2020-09-20 23:53:33 -0500

    alineando grid

--- src/Component/Skills/Skills.jsx
+++ src/Component/Skills/Skills.jsx
@@ -1,5 +1,5 @@
 import React  from 'react';
-import './Skills.css';
+import './Skills.scss';
 
 function Skills(){
 
@@ -7,13 +7,13 @@ function Skills(){
       <section className="container-skills">
         <h2 className="subtitle">Skills</h2>
         <div className="skills">
-        <img src={require('../../images/1.png')} alt="icon_html" className="skills-icons"></img>
-        <img src={require('../../images/2.png')} alt="icon_css" className="skills-icons"></img>
-        <img src={require('../../images/3.png')} alt="icon_javascript" className="skills-icons"></img>
-        <img src={require('../../images/4.png')} alt="icon_sass" className="skills-icons"></img>
-        <img src={require('../../images/5.png')} alt="icon_reactjs" className="skills-icons"></img>
-        <img src={require('../../images/6.png')} alt="icon_angular" className="skills-icons"></img>
-        <img src={require('../../images/7.png')} alt="icon_firebase" className="skills-icons"></img>
+        <img src={require('../../images/icon_html.png')} alt="icon_html" className="skills-icons"></img>
+        <img src={require('../../images/icon_css.png')} alt="icon_css" className="skills-icons"></img>
+        <img src={require('../../images/icon_sass.png')} alt="icon_javascript" className="skills-icons"></img>
+        <img src={require('../../images/icon_js.png')} alt="icon_sass" className="skills-icons"></img>
+        <img src={require('../../images/icon_react.png')} alt="icon_reactjs" className="skills-icons"></img>
+        <img src={require('../../images/icon_angular.png')} alt="icon_angular" className="skills-icons"></img>
+        <img src={require('../../images/icon_firebase.png')} alt="icon_firebase" className="skills-icons"></img>
         <img src={require('../../images/icon_git.png')} alt="icon_git" className="skills-icons"></img>
         <img src={require('../../images/icon_nodejs.png')} alt="icon_nodejs" className="skills-icons"></img>
         <img src={require('../../images/icon_jest.png')} alt="icon_jest" className="skills-icons"></img>
