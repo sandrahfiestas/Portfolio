3d10115f0ad477989b2461d4d32cc7b5b7f2f7ae
sandrahfiestas <shfiestas@gmail.com>
2020-09-24 03:06:00 -0500

    eliminando comentarios adicionales

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
 import Navbar from './Component/Navbar/Navbar';
+import Hello from './Component/Hello/Hello';
 import About from './Component/About/About';
 import Skills from './Component/Skills/Skills';
 import Projects from './Component/Projects/Projects';
@@ -10,29 +9,15 @@ import Footer from './Component/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
+      <Hello />
       <About />
       <Skills />
       <Projects />
       <Contact />
       <Footer />
-      
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+    </>
   );
 }
 
