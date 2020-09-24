53cc05e6cc9db6211ac39053a53b5f03120face0
sandrahfiestas <shfiestas@gmail.com>
2020-09-24 03:17:32 -0500

    eliminando comentarios adicionales

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -4,10 +4,6 @@ import './Navbar.scss';
 function Navbar(){
   return(
     <nav>
-      {/* <input type="checkbox" id="check"/>
-      <label for="check" className="checkbtn">
-        <img src={require('../../images/menu_burger.png')} className="fas fa-bars btn-burger" alt="burger" />
-      </label> */}
       <a className="a-logo" href="#"><img src={require('../../images/logo.png')} className="logo" alt="logo" /></a>
       <ul className="uli">
         <li><a href="#about">SOBRE MI</a></li>
