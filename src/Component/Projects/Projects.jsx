28bda4e38ece489a6f5834321a853b9a37afc00b
sandrahfiestas <shfiestas@gmail.com>
2020-09-24 03:20:13 -0500

    condicional para name y linkWeb de proyectos

--- src/Component/Projects/Projects.jsx
+++ src/Component/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './Projects.css';
+import './Projects.scss';
 
 
 function Projects(){
@@ -11,14 +11,6 @@ function Projects(){
           res => setProjects(res.data)
       )}, [setProjects])
 
-              {/* <div className="subtitle">
-                <h2>Skills</h2>
-                <ul className="about-skills">
-                    {data.skills && data.skills.map((skill, index) =>
-                        <p className="skill">{skill}</p>
-                    )}
-                </ul>
-            </div> */}
 
   return(
     <section id="project" className="container-projects">
@@ -28,26 +20,30 @@ function Projects(){
         {projects.length && projects.map((project, index) => 
           <div className="card-project">
 
-            <div className="card-image">
-              <img src={project.image} className="img-project" alt={project.title}></img>
+            <div className="card-image gallery">
+            <a href={project.image} className="a-img" data-lightbox="roadtrip" data-title={project.title}><img src={project.image} className="img-project" alt={project.title}></img></a>             
             </div>
           
             <div className="card-information">
-              <div className="div-information">
-                <h1>{project.title}</h1>
-                <h3>{project.name}</h3>
+              <div className="div-information"> 
+                <h1 className="title-project">{project.title}</h1>
+                {project.name.length ? <h2>"{project.name}"</h2> : ""}
                 <p className="p-information">{project.description}</p>
                 <p className="p-information">{project.technology}</p>
               </div>
               <div>
-                <button className="btn-project"><a target="_blank"  href={project.linkWeb}>DEMO</a></button>
+              {project.linkWeb.length 
+                ?
+                  <button className="btn-project"><a target="_blank"  href={project.linkWeb}>DEMO</a></button>
+                :
+                  "" 
+              }
                 <button className="btn-project"><a target="_blank"  href={project.linkRepo}>CÓDIGO</a></button>
               </div>
             </div>
           </div>
         )}
       </div>
- 
     </section>
   )
   };
