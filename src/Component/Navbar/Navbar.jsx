1dfdf7c5df34436fbc3f303b09f242e9e2c6caca
sandrahfiestas <shfiestas@gmail.com>
2020-12-04 23:25:09 -0500

    reestructurando estilos y media queries

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -5,10 +5,10 @@ function Navbar(){
   return(
     <nav>
       <a className="a-logo" href="#"><img src={require('../../images/logo.png')} className="logo" alt="logo" /></a>
-      <ul className="uli">
-        <li><a href="#about">SOBRE MI</a></li>
-        <li><a href="#project">PROYECTOS</a></li>
-        <li><a href="#contact">CONTACTO</a></li>
+      <ul className="nav-uli">
+        <li className="nav-item"><a href="#about">SOBRE MI</a></li>
+        <li className="nav-item"><a href="#project">PROYECTOS</a></li>
+        <li className="nav-item"><a href="#contact">CONTACTO</a></li>
       </ul>
     </nav>
   ) 
