dc9c982131eb1a695644b5b47085217ae460a539
sandrahfiestas <shfiestas@gmail.com>
2020-12-07 03:03:08 -0500

    mostrando data con useEffect y fetch

--- src/Component/Projects/Projects.jsx
+++ src/Component/Projects/Projects.jsx
@@ -1,49 +1,52 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import './Projects.scss';
 
 
 function Projects(){
   const [projects, setProjects] = useState({});
-  useEffect(() => {
-      axios.get('https://backend-portfolio-shfiestas.herokuapp.com/portfolio')
-      .then(
-          res => setProjects(res.data)
-      )}, [setProjects])
+  // useEffect(() => {
+  //     axios.get('https://backend-portfolio-shfiestas.herokuapp.com/portfolio')
+  //     .then(
+  //         res => setProjects(res.data)
+  //     )}, [setProjects])
 
+  useEffect(() => {
+    fetch('../../portfolio.json')
+    .then((response) => response.json())
+    .then((datos) => {
+      setProjects(datos);
+    });
+  });
 
   return(
     <section id="project" className="container-projects">
-    <h2 className="subtitle">Proyectos</h2>
- 
-      <div className="main-card">
+      <h2 className="subtitle">Proyectos</h2>
         {projects.length && projects.map((project, index) => 
           <div className="card-project">
-
-            <div className="card-image gallery">
-            <a href={project.image} className="a-img" data-lightbox="roadtrip" data-title={project.title}><img src={project.image} className="img-project" alt={project.title}></img></a>             
+            <div className="div-image">
+            <a href={project.image} className="a-img" data-lightbox="roadtrip" data-title={project.title}><img src={project.image} className="image-project" alt={project.title}></img></a>             
             </div>
           
-            <div className="card-information">
-              <div className="div-information"> 
+            <div className="div-detail">
+              <div> 
                 <h1 className="title-project">{project.title}</h1>
-                {project.name.length ? <h2>"{project.name}"</h2> : ""}
+                {project.name.length ? <h2 className="sub-title">"{project.name}"</h2> : ""}
                 <p className="p-information">{project.description}</p>
                 <p className="p-information">{project.technology}</p>
               </div>
-              <div>
-              {project.linkWeb.length 
-                ?
-                  <button className="btn-project"><a target="_blank"  href={project.linkWeb}>DEMO</a></button>
-                :
-                  "" 
-              }
-                <button className="btn-project"><a target="_blank"  href={project.linkRepo}>CÓDIGO</a></button>
+              <div className="div-btn">
+                 {project.linkWeb.length 
+                    ?
+                    <button className="base_buttons"><a target="_blank"  href={project.linkWeb}>DEMO</a></button>
+                    :
+                    "" 
+                 }
+                 <button className="base_buttons"><a target="_blank"  href={project.linkRepo}>CÓDIGO</a></button>
               </div>
             </div>
           </div>
         )}
-      </div>
     </section>
   )
   };
