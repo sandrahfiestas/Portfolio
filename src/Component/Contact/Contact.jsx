f1b0eed684c399dfbb035d501f457d54705fa61e
sandrahfiestas <shfiestas@gmail.com>
2020-12-09 23:53:39 -0500

    reestructurando componente contact

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -4,34 +4,38 @@ import './Contact.scss';
 function Contact(){
   return(
     <section id="contact" className="container-contact">
-      <p className="subtitle-dos">¿Listo para hablar sobre trabajar juntos?</p>
-      <h1 className="title-contact">CONTÁCTAME</h1>
-      <div className="networks">
-      
-        <div className="network-uno">
-        <a className="a-contact" target="_blank"  href="mailto:shfiestas@gmail.com">
-            <img src={require('../../images/icon-gmail.png')} alt="icon-gmail" className="icon-network-uno"></img>
-            <p className="p-network">Email Me</p>
-        </a>
+        <p className="subtitle-two">¿Listo para hablar sobre trabajar juntos?</p>
+        <h1 className="title-contact">CONTÁCTAME</h1>
+        <div className="networking">
+
+        <div className="btn-network">
+          <a className="a-network" target="_blank"  href="mailto:shfiestas@gmail.com">
+              <div className="div-icon-network">
+               <img src={require('../../images/icon-gmail.png')} alt="icon-gmail" className="icon-network"></img>
+              </div>
+              <p className="p-network">Email Me</p>
+          </a>
         </div>
       
-
-        <div className="network-dos">
-        <a className="a-contact" target="_blank"  href="https://www.linkedin.com/in/sandrahuam%C3%A1nfiestas/">
-          <img src={require('../../images/icon-linkendin.png')} alt="icon-linkendIn" className="icon-network-dos"></img>
-          <p className="p-network">LinkendIn</p>
-        </a>
+        <div className="btn-network">
+          <a className="a-network" target="_blank"  href="https://www.linkedin.com/in/sandrahuam%C3%A1nfiestas/">
+              <div className="div-icon-network">
+                <img src={require('../../images/icon-linkendin.png')} alt="icon-linkendIn" className="icon-network"></img>
+              </div>
+              <p className="p-network">LinkendIn</p>
+          </a>
         </div>
 
-        <div className="network-tres">
-        <a className="a-contact" target="_blank"  href="https://github.com/sandrahfiestas">
-          <img src={require('../../images/icon_github.png')} alt="icon-github" className="icon-network-tres"></img>
-          <p className="p-network">GitHub</p>
-        </a>
+        <div className="btn-network">
+         <a className="a-network" target="_blank"  href="https://github.com/sandrahfiestas">
+            <div className="div-icon-network">
+              <img src={require('../../images/icon_github.png')} alt="icon-github" className="icon-network"></img>
+            </div>
+            <p className="p-network">GitHub</p>
+          </a>
         </div>
       </div>
     </section>
-    
   )
 }
 
